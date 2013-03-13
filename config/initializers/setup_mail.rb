3dcef5c07440ce7755219931dcf3779d2644f292
Kevin Chen <kchen508@gmail.com>
2013-03-13 01:10:39 -0400

    fix commit

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => "gmail.com",
-    :user_name => "forsakenworldvampire@gmail.com",
-    :password => "discoparty",
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"],
     :authentication => "plain",
     :enable_starttls_auto => true
 }
